7f75ecc620ddfd4014c93939ecdddda755828f19
Ganesh Rao <ganeshnrao@me.com>
2019-11-10 21:35:00 -0800

    Open each execution result in separate file, make API URL configurable

--- src/extension.ts
+++ src/extension.ts
@@ -1,92 +1,120 @@
 import * as vscode from "vscode";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-const editorMap: Map<vscode.TextEditor, vscode.TextEditor> = new Map();
-const apiUrl: string = "http://localhost:2020/api";
-const loading: string = JSON.stringify({ message: "Loading" }, null, "  ");
+const maxContentLength = 1000000;
+
+const logger = {
+  log(...args: any): void {
+    console.log("{ MonCow }", ...args);
+  }
+};
 
 async function createNewEditor(
   content: string = "",
-  language: string = "json"
+  language: string = "jsonc"
 ): Promise<vscode.TextEditor> {
   const document = await vscode.workspace.openTextDocument({
     content,
     language
   });
+  logger.log("Document created");
   const editor = await vscode.window.showTextDocument(
     document,
     vscode.ViewColumn.Beside,
     true
   );
+  logger.log("Editor created");
   return editor;
 }
 
-async function getLinkedEditor(
+function getMetaData(originEditor: vscode.TextEditor) {
+  return {
+    sourceFile: originEditor.document.uri.path,
+    ranAt: new Date().toISOString()
+  };
+}
+
+async function runCommand(
+  code: string = "",
   originEditor: vscode.TextEditor
-): Promise<vscode.TextEditor> {
-  const editor: vscode.TextEditor | undefined = editorMap.get(originEditor);
-  if (editor && vscode.window.visibleTextEditors.includes(editor)) {
-    return editor;
+): Promise<string> {
+  const { sourceFile, ranAt } = getMetaData(originEditor);
+  try {
+    const requestConfig: AxiosRequestConfig = {
+      method: "post",
+      url: "/run",
+      data: { code }
+    };
+    const {
+      data: { environments, result, logs }
+    } = await axios.request(requestConfig);
+    const data = { sourceFile, ranAt, environments, result, logs };
+    return JSON.stringify(data, null, "  ");
+  } catch (error) {
+    const {
+      data: { errorMessage = "Failed to execute code", stack = [] } = {}
+    } = error.response || {};
+    return [
+      `sourceFile: ${sourceFile}`,
+      `ranAt: ${ranAt}\n`,
+      errorMessage,
+      ...stack
+    ].join("\n");
   }
-  const newEditor = await createNewEditor();
-  editorMap.set(originEditor, newEditor);
-  return newEditor;
 }
 
-async function replaceText(
-  editor: vscode.TextEditor,
-  content: string = ""
-): Promise<vscode.TextEditor> {
-  const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-  const firstLine = editor.document.lineAt(0);
-  await editor.edit((editBuilder: vscode.TextEditorEdit) => {
-    const range = new vscode.Range(firstLine.range.start, lastLine.range.end);
-    editBuilder.replace(range, content);
-  });
-  return editor;
-}
-
-async function showResult(
-  originEditor: vscode.TextEditor,
-  content: string = ""
-): Promise<vscode.TextEditor> {
-  const linkedEditor = await getLinkedEditor(originEditor);
-  await replaceText(linkedEditor, content);
-  return linkedEditor;
-}
-
-async function runCommand(code: string = ""): Promise<string> {
-  const { data } = await axios.request({
-    method: "post",
-    url: `${apiUrl}/run`,
-    data: { code }
-  });
-  return JSON.stringify(data, null, "  ");
+function truncateContent(content: string): string {
+  if (content.length > maxContentLength) {
+    const truncated = content.slice(0, maxContentLength);
+    logger.log(
+      `Result is ${content.length} characters, truncated to ${maxContentLength}`
+    );
+    return `// truncated to first ${maxContentLength} characters\n${truncated}`;
+  }
+  return content;
 }
 
 async function runFile(originEditor: vscode.TextEditor): Promise<void> {
   try {
-    const code = originEditor.document.getText();
-    if (code.indexOf("// moncow") !== 0) {
+    const code = originEditor.document.getText().trim();
+    if (!code.startsWith("// moncow")) {
       vscode.window.showInformationMessage(
         "File will not be executed unless it starts with `// moncow`"
       );
       return;
     }
-    await showResult(originEditor, loading);
-    const result = await runCommand(code);
-    await showResult(originEditor, result);
+    logger.log(`Run file ${originEditor.document.uri.path}`);
+    const content = truncateContent(await runCommand(code, originEditor));
+    await createNewEditor(content);
   } catch (error) {
+    logger.log("Failed to run file", error.stack);
     vscode.window.showInformationMessage(
-      `Failed to execute file ${JSON.stringify(error.stack)}`
+      `Failed to run file. ${error.message}`
     );
   }
 }
 
-export function activate(): void {
-  vscode.commands.registerTextEditorCommand("moncow.runFile", runFile);
+function setAxiosBase(): void {
+  axios.defaults.baseURL = vscode.workspace
+    .getConfiguration()
+    .get("moncow.apiUrl");
+  logger.log(`Set base URL ${axios.defaults.baseURL}`);
 }
 
-export function deactivate(): void {
-  console.log("MonCow deactivated");
+export function activate({ subscriptions }: vscode.ExtensionContext): void {
+  setAxiosBase();
+  subscriptions.push(
+    vscode.commands.registerTextEditorCommand("moncow.runFile", runFile)
+  );
+  subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration(
+      (event: vscode.ConfigurationChangeEvent) => {
+        if (event.affectsConfiguration("moncow.apiUrl")) {
+          setAxiosBase();
+        }
+      }
+    )
+  );
 }
+
+export function deactivate(): void {}
